e456d9548c87a8c11937d36ffccb5a4954aa6d35
poslam <kirden04@gmail.com>
2023-12-03 19:09:02 +1000

    rating system

--- backend/api/src/v1/rating.py
+++ backend/api/src/v1/rating.py
@@ -11,18 +11,13 @@ rating_router = APIRouter()
 
 @rating_router.post("/add_result")
 async def rating_add_result(result: RatingIn,
-                            user_in=Depends(login_required),
+                            user=Depends(login_required),
                             session: AsyncSession = Depends(get_session)):
 
     text = await session.get(Text, result.text_id)
 
     if text == None:
         raise HTTPException(status_code=400, detail="text not found")
-
-    user = await session.get(User, result.user_id)
-
-    if user == None:
-        raise HTTPException(status_code=400, detail="user not found")
 
     result_ins = {
         "text_id": result.text_id,
@@ -43,28 +38,77 @@ async def rating_add_result(text_id: int = None,
                             user_in=Depends(login_required),
                             session: AsyncSession = Depends(get_session)):
 
+    result = []
+
     if text_id == None and tier == None:
 
-        pass
+        users = (await session.execute(select(User.id)))
+
+        for user_raw in users:
+
+            user_id = user_raw._mapping["id"]
+
+            result.append([x._mapping for x in (await session.execute(
+                select(Result.id.label("result_id"),
+                       User.nickname,
+                       Text.id.label("text_id"),
+                       Result.speed,
+                       Result.correct_rate,
+                       (Tier.speed_ratio*Result.speed +
+                        Tier.correct_rate_ratio*Result.correct_rate).label("score"))
+                .where(Result.text_id == Text.id)
+                .where(Text.tier == Tier.id)
+                .where(Result.user_id == User.id)
+                .where(Result.user_id == user_id)
+                .order_by(desc("score"))))][0])
 
     elif text_id != None and tier == None:
 
-        users = (await session.execute(select(Result).where(Result.text_id == text_id)))
+        users = (await session.execute(select(Result.user_id).where(Result.text_id == text_id))).unique().all()
 
-        result = [x._mapping for x in (await session.execute(
-            select(Result.id.label("result_id"),
-                   User.nickname,
-                   (Tier.speed_ratio*Result.speed +
-                    Tier.correct_rate_ratio*Result.correct_rate).label("score"))
-            .where(Result.text_id == Text.id)
-            .where(Text.tier == Tier.id)
-            .where(Result.user_id == User.id)
-            .where(Text.id == text_id)
-            .order_by(desc("score"))))]
+        for user_raw in users:
+
+            user_id = user_raw._mapping["user_id"]
+
+            result.append([x._mapping for x in (await session.execute(
+                select(Result.id.label("result_id"),
+                       User.nickname,
+                       Result.speed,
+                       Result.correct_rate,
+                       (Tier.speed_ratio*Result.speed +
+                        Tier.correct_rate_ratio*Result.correct_rate).label("score"))
+                .where(Result.text_id == Text.id)
+                .where(Text.tier == Tier.id)
+                .where(Result.user_id == User.id)
+                .where(Result.user_id == user_id)
+                .where(Text.id == text_id)
+                .order_by(desc("score"))))][0])
 
     elif text_id == None and tier != None:
 
-        pass
+        users = (await session.execute(select(Result.user_id)
+                                       .where(Result.text_id == Text.id)
+                                       .where(Text.tier == Tier.id)
+                                       .where(Tier.id == tier))).unique().all()
+
+        for user_raw in users:
+
+            user_id = user_raw._mapping["user_id"]
+
+            result.append([x._mapping for x in (await session.execute(
+                select(Result.id.label("result_id"),
+                       User.nickname,
+                       Text.id.label("text_id"),
+                       Result.speed,
+                       Result.correct_rate,
+                       (Tier.speed_ratio*Result.speed +
+                        Tier.correct_rate_ratio*Result.correct_rate).label("score"))
+                .where(Result.text_id == Text.id)
+                .where(Text.tier == Tier.id)
+                .where(Tier.id == tier)
+                .where(Result.user_id == User.id)
+                .where(Result.user_id == user_id)
+                .order_by(desc("score"))))][0])
 
     else:
         raise HTTPException(status_code=400, detail="wrong parameters")
