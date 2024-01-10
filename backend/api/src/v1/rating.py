91849595a275f69a393f49fb29eabe9208542367
Кириенко Денис <kirden04@gmail.com>
2024-01-10 11:52:46 +1000

    fix

--- backend/api/src/v1/rating.py
+++ backend/api/src/v1/rating.py
@@ -52,20 +52,22 @@ async def rating_add_result(text_id: int = None,
             for user_raw in users:
 
                 user_id = user_raw._mapping["id"]
-
-                result.append([x._mapping for x in (await session.execute(
-                    select(Result.id.label("result_id"),
-                           User.nickname,
-                           Text.id.label("text_id"),
-                           Result.speed,
-                           Result.correct_rate,
-                           (Tier.speed_ratio*Result.speed +
-                            Tier.correct_rate_ratio*Result.correct_rate).label("score"))
-                    .where(Result.text_id == Text.id)
-                    .where(Text.tier == Tier.id)
-                    .where(Result.user_id == User.id)
-                    .where(Result.user_id == user_id)
-                    .order_by(desc("score"))))][0])
+                try:
+                    result.append([x._mapping for x in (await session.execute(
+                        select(Result.id.label("result_id"),
+                            User.nickname,
+                            Text.id.label("text_id"),
+                            Result.speed,
+                            Result.correct_rate,
+                            (Tier.speed_ratio*Result.speed +
+                                Tier.correct_rate_ratio*Result.correct_rate).label("score"))
+                        .where(Result.text_id == Text.id)
+                        .where(Text.tier == Tier.id)
+                        .where(Result.user_id == User.id)
+                        .where(Result.user_id == user_id)
+                        .order_by(desc("score"))))][0])
+                except:
+                    pass
 
         elif text_id != None and tier == None:
 
@@ -103,20 +105,23 @@ async def rating_add_result(text_id: int = None,
 
                 user_id = user_raw._mapping["user_id"]
 
-                result.append([x._mapping for x in (await session.execute(
-                    select(Result.id.label("result_id"),
-                           User.nickname,
-                           Text.id.label("text_id"),
-                           Result.speed,
-                           Result.correct_rate,
-                           (Tier.speed_ratio*Result.speed +
-                            Tier.correct_rate_ratio*Result.correct_rate).label("score"))
-                    .where(Result.text_id == Text.id)
-                    .where(Text.tier == Tier.id)
-                    .where(Tier.id == tier)
-                    .where(Result.user_id == User.id)
-                    .where(Result.user_id == user_id)
-                    .order_by(desc("score"))))][0])
+                try:
+                    result.append([x._mapping for x in (await session.execute(
+                        select(Result.id.label("result_id"),
+                            User.nickname,
+                            Text.id.label("text_id"),
+                            Result.speed,
+                            Result.correct_rate,
+                            (Tier.speed_ratio*Result.speed +
+                                Tier.correct_rate_ratio*Result.correct_rate).label("score"))
+                        .where(Result.text_id == Text.id)
+                        .where(Text.tier == Tier.id)
+                        .where(Tier.id == tier)
+                        .where(Result.user_id == User.id)
+                        .where(Result.user_id == user_id)
+                        .order_by(desc("score"))))][0])
+                except:
+                    pass
 
         else:
             raise HTTPException(status_code=400, detail="wrong parameters")
